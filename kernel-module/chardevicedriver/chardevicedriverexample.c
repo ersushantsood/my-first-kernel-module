fa0072421371326a27ded5568c7187405ba73f81
samitasood <sam85maverick@gmail.com>
2023-11-05 14:44:26 -0800

    implemented char device driver

--- kernel-module/chardevicedriver/chardevicedriverexample.c
+++ kernel-module/chardevicedriver/chardevicedriverexample.c
@@ -44,8 +44,8 @@ static char msg[BUF_LEN + 1]; /* The msg the device will give when asked */
 static struct class *cls;
 
 /*function pointers read,write,open,release in file_operations struct called
-* by kernel syscall read,write,open,release pointing to mychardev driver functions 
-*device_read, device_write,device_open, device_release */
+ *by kernel when a process tries to open the device file, like "sudo cat /dev/mychardev" 
+ *pointing to mychardev driver functions device_read, device_write,device_open, device_release */
 static struct file_operations mychardev_fops = {
     .read = device_read,
     .write = device_write,
@@ -68,7 +68,8 @@ static int __init mychardev_init(void)
     either make the device file by hand or write a shell script to read the file in and
     make the device file. The third method is that we can have our driver make the
     device file using the device_create function after a successful registration and
-    device_destroy during the call to cleanup_module.*/
+    device_destroy during the call to cleanup_module.
+    */
     major = register_chardev(0, DEVICE_NAME, &mychardev_fops);
 
     if (major < 0) {
@@ -87,7 +88,7 @@ static int __init mychardev_init(void)
 }
 
 // driver module cleanup function starts
-static void __exit chardev_exit(void)
+static void __exit mychardev_exit(void)
 {
     device_destroy(cls, MKDEV(major, 0));
     class_destroy(cls);
@@ -103,3 +104,83 @@ static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char __user *, size_t,loff_t *);
 */
 
+/* Called when a process tries to open the device file, like "sudo cat /dev/mychardev"
+*/
+
+static int device_open(struct inode *inode, struct file *file)
+{
+    static int counter = 0;
+
+    if (atomic_cmpxchg(&already_open, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN))
+        return -EBUSY;  
+
+    sprintf(msg, "I already told you %d times Hello world!\n", counter++);
+    try_module_get(THIS_MODULE);
+
+    return SUCCESS; 
+}
+
+/* Called when a process closes the device file. */
+static int device_release(struct inode *inode, struct file *file)
+{
+    /* We're now ready for our next caller */
+    atomic_set(&already_open, CDEV_NOT_USED);
+
+    /* Decrement the usage count, or else once you opened the file, you will
+     * never get rid of the module.
+    */
+    module_put(THIS_MODULE);
+
+    return SUCCESS;
+}
+
+/* Called when a process, which already opened the dev file, attempts to
+ * read from it.
+*/
+static ssize_t device_read(struct file *filp, /* see include/linux/fs.h */
+                           char __user *buffer, /* buffer to fill with data */
+                           size_t length, /* length of the buffer */
+                           loff_t *offset)
+{
+    /* Number of bytes actually written to the buffer */
+    int bytes_read = 0;
+    const char *msg_ptr = msg;
+
+    if (!*(msg_ptr + *offset)) { /* we are at the end of message */
+        *offset = 0; /* reset the offset */
+        return 0; /* signify end of file */
+    }
+
+    msg_ptr += *offset;
+
+    /* Actually put the data into the buffer */
+    while (length && *msg_ptr) {
+        /* The buffer is in the user data segment, not the kernel
+         * segment so "*" assignment won't work. We have to use
+         * put_user which copies data from the kernel data segment to
+         * the user data segment.
+         */
+        put_user(*(msg_ptr++), buffer++);
+        length--;
+
+        bytes_read++;
+    }
+
+    *offset += bytes_read;
+
+    /* Most read functions return the number of bytes put into the buffer. */
+    return bytes_read;
+}
+
+/* Called when a process writes to dev file: echo "hi" > /dev/hello */
+static ssize_t device_write(struct file *filp, const char __user *buff,
+                            size_t len, loff_t *off)
+{
+    pr_alert("Sorry, this operation is not supported.\n");
+    return -EINVAL;
+}
+
+module_init(mychardev_init);
+module_exit(mychardev_exit);
+
+MODULE_LICENSE("GPL");
